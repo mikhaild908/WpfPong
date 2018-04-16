3296ba8838e52a49fba3f36c743a1d412e1d1beb
Mike Dumlao <Mike.Dumlao@neudesic.com>
2018-04-16 16:12:34 -0700

    forgot to add this file

---
+++ WpfPong/Model/Ball.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPong.Model
+{
+    public class Ball
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+    }
+}
